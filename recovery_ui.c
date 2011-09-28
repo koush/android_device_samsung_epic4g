6668e6088dbf224b8e1119455929669942649f7b
Donovan Bartish <djdonovanbartish@yahoo.com>
2011-09-28 03:11:13 -0500

    Added Power Off to menu, and switched back to home softkey for select.
    Change-Id: I59e70ebb412240fb259dfaf9e70aaf18b0364216

--- recovery_ui.c
+++ recovery_ui.c
@@ -20,7 +20,7 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { "Use vol keys to highlight and menu to select.",
+char* MENU_HEADERS[] = { "Use vol keys to highlight and home to select.",
                          "",
                          NULL };
 
@@ -32,21 +32,15 @@ char* MENU_ITEMS[] = { "reboot system now",
                        "backup and restore",
                        "mounts and storage",
                        "advanced",
-                       NULL };
+                       "power off",
+			NULL };
 
 int device_recovery_start() {
     return 0;
 }
 
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+int device_toggle_display(volatile char* key_pressed, int key_code) { // hold power and press volume-up 
+	return key_pressed[KEY_POWER] && key_code == KEY_VOLUMEUP; 
 }
 
 int device_reboot_now(volatile char* key_pressed, int key_code) {
@@ -64,7 +58,7 @@ int device_handle_key(int key_code, int visible) {
             case 49:
                 return HIGHLIGHT_DOWN;
 
-            case 158:
+            case 139:
             case 46:
             case 40:
                 return SELECT_ITEM;
