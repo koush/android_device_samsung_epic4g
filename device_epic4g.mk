dbbddc13472dd5eb65cd7a4b3ed7c58ccd740c2f
Donovan Bartish <djdonovanbartish@yahoo.com>
2011-10-02 13:32:38 -0500

    Revert "add keychars and keylayout"
    This reverts commit 327a258a8bf7a689c5f2caaaf6841cb6eb0240b9.

--- device_epic4g.mk
+++ device_epic4g.mk
@@ -7,19 +7,6 @@ $(call inherit-product-if-exists, vendor/samsung/epic4g/epic4g-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/epic4g/overlay
 
-# Keylayout and Keychars
-PRODUCT_COPY_FILES += \
-     device/samsung/epic/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
-     device/samsung/epic/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
-     device/samsung/epic/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-     device/samsung/epic/prebuilt/keylayout/qt602240_ts_input.kl:system/usr/keylayout/qt602240_ts_input.kl \
-     device/samsung/epic/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
-     device/samsung/epic/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-     device/samsung/epic/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
-     device/samsung/epic/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
-     device/samsung/epic/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-     device/samsung/epic/prebuilt/keychars/qt602240_ts_input.kcm.bin:system/usr/keychars/qt602240_ts_input.kcm.bin \
-     device/samsung/epic/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/samsung/epic4g/kernel
